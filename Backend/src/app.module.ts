f0fb4a0e68dbab8e797f092473c312919be78ef1
Safaa <Safaeettalhi@gmail.com>
2026-02-02 20:55:48 +0100

    ST1.3.1 ST1.3.2: Schéma User et UsersService avec MongoDB

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import{ConfigModule} from '@nestjs/config';
 import { AppController } from './app.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI ?? 'mongodb://localhost:27017/evenza'),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
