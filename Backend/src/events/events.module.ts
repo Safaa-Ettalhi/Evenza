06860277fc0a97062d044b4f208c4c29f27480c5
Safaa <Safaeettalhi@gmail.com>
2026-02-06 22:19:54 +0100

    importer le modèle Reservation pour calcul des places disponibles

--- Backend/src/events/events.module.ts
+++ Backend/src/events/events.module.ts
@@ -3,10 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { EventsService } from './events.service';
 import { EventsController } from './events.controller';
 import { Event, EventSchema } from './event.schema';
+import { Reservation, ReservationSchema } from '../reservations/reservation.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Event.name, schema: EventSchema }]),
+    MongooseModule.forFeature([
+      { name: Event.name, schema: EventSchema },
+      { name: Reservation.name, schema: ReservationSchema },
+    ]),
   ],
   controllers: [EventsController],
   providers: [EventsService],
